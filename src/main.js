1c219e1a26d9e6d2bf945208846f5bacf9379066
Serge S <sergey@shushpanov.me>
2019-07-02 16:06:34 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './vuex/store'
+import Axios from 'axios'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  create () {
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+    Axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
+  },
+  render: h => h(App)
+}).$mount('#app')
